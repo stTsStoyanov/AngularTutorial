968e6813947f4fea6560a87f66edaf98ea35308b
unknown <st.ts.stoyanov@gmail.com>
2023-09-01 16:05:45 +0300

    lesson 10 is done, the app has routing!

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -7,17 +8,30 @@ import { HomeComponent } from './home/home.component';
   standalone: true,
   imports: [
     HomeComponent,
+    RouterModule,
   ],
   template: `
-    <main>
+  <main>
+    <a [routerLink]="['/']">
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
+  // template: `
+  //   <main>
+  //     <header class="brand-name">
+  //       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+  //     </header>
+  //     <section class="content">
+  //       <app-home></app-home>
+  //     </section>
+  //   </main>
+  // `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -3,8 +3,16 @@
 *  Protractor is used in this example for compatibility with Angular documentation tools.
 */
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
