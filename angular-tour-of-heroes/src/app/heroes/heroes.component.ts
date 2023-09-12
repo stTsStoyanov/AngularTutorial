3bd6c2f71e557fa60b6ab40593f63338d121a4a4
unknown <st.ts.stoyanov@gmail.com>
2023-09-12 14:35:12 +0300

    added asynchronous code, with RxJS Library, using Observable, subscribe() and handling the response in promise callback

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
-import { Component, Output } from '@angular/core';
+import { Component, Inject, Output } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,11 +16,37 @@ export class HeroesComponent {
   //   name: 'Windstorm'
   // };
 
-  heroes = HEROES;
+
+  //When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService.
+  // heroService become instance of HeroService!
+  constructor(private heroService: HeroService){ 
+    
+  }
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero){
     this.selectedHero = hero;
   }
   @Output() selected = this.selectedHero;
+
+  //old synchronous version
+  // getHeroes(){
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(){
+    this.heroService.getHeroes()
+    //return promise in the callback
+      .subscribe(heroes =>{
+        this.heroes = heroes;
+      })
+  }
+
+  //getHeroes() inside the ngOnInit lifecycle hook let Angular call ngOnInit() at an appropriate time after constructing a HeroesComponent instance.
+  // when the component is created, get all the heroes from the service and inject them in heroes prop, which is used to render the list
+  ngOnInit(){
+    this.getHeroes();
+  }
 }
